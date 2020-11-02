cf829441a5ee90badcc830e76c4c84081d1a5634
Eustache <eustacheeussy@gmail.com>
2020-11-02 20:17:54 +0200

    this is  login

--- server.js
+++ server.js
@@ -25,5 +25,21 @@ app.post('/users', async(req, res)=>{
     }
 });
 
+app.post('/users/login', async (req,res)=>{
+    const user=users.find(user=>user.name=req.body.name);
+    if(user==null){
+        return res.status(400).send('can not find user');
+    }try{
+        if( await bcrypt.compare(req.body.password, user.password)){
+            res.send('you have successful logged in');
+        }else{
+            res.send('Incorrect username or password'); 
+        }
+    }catch{
+        res.status(500).send();
+    }
+
+});
+
 const port=process.env.port || 3000
 app.listen(port,()=>{console.log(`server starting on port: ${port}...`)});
