5caf75bbbb460e9cb25f6997a0a57fc6001d79cb
Eustache <eustacheeussy@gmail.com>
2020-11-04 23:31:12 +0200

    removing comments

--- server.js
+++ server.js
@@ -14,8 +14,6 @@ app.post('/users', async(req, res)=>{
     try{
         const salt = await bcrypt.genSalt();
         const hashedPassword= await bcrypt.hash(req.body.password, salt);
-        // console.log(salt);
-        // console.log(hashedPassword);
         const user= {name:req.body.name, password:hashedPassword};
         users.push(user);
         res.status(201).send();
